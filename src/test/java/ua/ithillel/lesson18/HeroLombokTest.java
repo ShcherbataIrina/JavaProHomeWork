00a735bf0ae0a4d7697692e2be4fdb19ecbb82af
ShcherbataIrina <serbataairina@gmail.com>
2023-03-30 13:34:23 +0300

    hw17_Lombok

--- src/test/java/ua/ithillel/lesson18/HeroLombokTest.java
+++ src/test/java/ua/ithillel/lesson18/HeroLombokTest.java
@@ -1,17 +1,72 @@
 package ua.ithillel.lesson18;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HeroLombokTest {
+    private HeroLombok heroLombok;
+
+    @BeforeEach
+    void setUp() {
+        heroLombok = new HeroLombok();
+        heroLombok.setName("Batman");
+        heroLombok.setGender("Male");
+        heroLombok.setAlignment("good");
+        heroLombok.setSkinColor("-");
+        heroLombok.setEyeColor("Blue");
+        heroLombok.setPublisher("Marvel Comics");
+        heroLombok.setHeight(189.4);
+        heroLombok.setRace("Human");
+        heroLombok.setWeight(78);
+        heroLombok.setHairColor("Brown");
+    }
 
     @Test
-    void shouldNameGet(){
-
-        HeroLombok heroLombok = new HeroLombok();
-
-        heroLombok.setName("Batman");
+    void shouldGetName() {
 
         Assertions.assertEquals("Batman", heroLombok.getName());
+    }
+
+    @Test
+    void shouldGetGender() {
+
+        Assertions.assertEquals("Male", heroLombok.getGender());
+    }
+
+    @Test
+    void shouldGetAlignment() {
+
+        Assertions.assertEquals("good", heroLombok.getAlignment());
+    }
+
+    @Test
+    void shouldGetEyeColor() {
+
+        Assertions.assertEquals("Blue", heroLombok.getEyeColor());
+    }
+
+    @Test
+    void shouldGetSkinColor() {
+
+        Assertions.assertEquals("-", heroLombok.getSkinColor());
+    }
+
+    @Test
+    void shouldGetRace() {
+
+        Assertions.assertEquals("Human", heroLombok.getRace());
+    }
+
+    @Test
+    void shouldGetWeight() {
+
+        Assertions.assertEquals(78, heroLombok.getWeight());
+    }
+
+    @Test
+    void shouldGetHeight() {
+
+        Assertions.assertEquals(189.4, heroLombok.getHeight());
     }
 }
--- src/test/java/ua/ithillel/lesson18/HeroValueTest.java
+++ src/test/java/ua/ithillel/lesson18/HeroValueTest.java
@@ -4,13 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HeroValueTest {
+class  HeroValueTest {
     @Test
-    void shouldGetName() {
+    void shouldGetItemHeroValue() {
 
         HeroValue heroValue = new HeroValue("Harry Potter", "Male", "Green", "Human",
                 "Brown", 173.5, "Comics CMD", "-", "good", 69);
 
         assertEquals("Harry Potter", heroValue.getName());
+        assertEquals("Male", heroValue.getGender());
+        assertEquals("good", heroValue.getAlignment());
+        assertEquals("Human", heroValue.getRace());
+        assertEquals("Comics CMD", heroValue.getPublisher());
+        assertEquals("Green", heroValue.getEyeColor());
+        assertEquals("Brown", heroValue.getHairColor());
+        assertEquals("-", heroValue.getSkinColor());
+        assertEquals(173.5, heroValue.getHeight());
+        assertEquals(69, heroValue.getWeight());
+
     }
 }
