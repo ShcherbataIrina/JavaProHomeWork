f01af4b2f951b9f9477739742b2a4b661cc3dad8
ShcherbataIrina <serbataairina@gmail.com>
2023-05-16 17:51:40 +0300

    hw26_Hibernate

---
+++ src/main/java/ua/ithillel/lesson30_hibernate/HeroHibernate.java
@@ -0,0 +1,40 @@
+package ua.ithillel.lesson30_hibernate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "heroes")
+public class HeroHibernate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    String name;
+    String gender;
+
+    @Column(name = "eye_color")
+    String eyeColor;
+    String race;
+
+    @Column(name = "hair_color")
+    String hairColor;
+    double height;
+
+    @Column(name = "skin_color")
+    String skinColor;
+    String alignment;
+    int weight;
+
+    @Column(name = "publisher_id")
+    Long publisherId;
+
+}
